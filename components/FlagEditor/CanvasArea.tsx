3216d1abdcfc0bf15bac61d610a73089a24f5499
Mariano <marianoarrigoni@outlook.com>
2025-04-07 00:57:34 +0200

    fix: text preview fix size

--- components/FlagEditor/CanvasArea.tsx
+++ components/FlagEditor/CanvasArea.tsx
@@ -228,6 +228,7 @@ const CanvasArea = ({
             setLastSelectedTarget('image');
             setSelectedTextId(null);
           }}
+          previewMode={previewMode}
         />
       ))}
     </AspectRatioBox>
